a82d6f7b31737bd42fa417fdac4a5117a0d8332b
pikesley <github@orgraphone.org>
2015-08-25 21:21:20 +0100

    THIS is how we TDD

--- lib/replacer_bot/searcher.rb
+++ lib/replacer_bot/searcher.rb
@@ -1,7 +1,7 @@
 module ReplacerBot
   class Searcher
-    def self.results
-      []
+    def self.results term: Config.instance.config.search_term, count: Config.instance.config.search_count
+      TwitterClient.instance.client.search(ReplacerBot.encode(term: term), result_type: 'recent').take(count)
     end
   end
 end
--- spec/lib/replacer_bot/searcher_spec.rb
+++ spec/lib/replacer_bot/searcher_spec.rb
@@ -1,7 +1,15 @@
 module ReplacerBot
   describe Searcher do
-    it 'returns search results' do
+    it 'returns something', :vcr do
       expect(described_class.results).to be_an Array
+    end
+
+    it 'returns tweets', :vcr do
+      expect(described_class.results.first).to be_a Twitter::Tweet
+    end
+
+    it 'returns kosher tweets', :vcr do
+      expect(described_class.results.first.text).to match /open ?data/i
     end
   end
 end
